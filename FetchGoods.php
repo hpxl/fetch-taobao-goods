386900d0f495002d94aa616587f736bccc4b8a18
hpxl <lvwl.cn@163.com>
2014-04-20 09:19:13 +0800

    add file

---
+++ FetchGoods.php
@@ -0,0 +1,357 @@
+<?php
+/**
+ * 采集商品，暂支持淘宝、天猫商品
+ */
+if (substr(php_sapi_name(), 0, 3) !== 'cli') {
+    die("This Programe can only be run in CLI mode\n");
+}
+
+// 记录进程是否在运行
+define("PIDFILE", '/tmp/fetchgoods.pid');
+file_put_contents(PIDFILE, getmypid());
+function removePidFile() {
+    unlink(PIDFILE);
+}
+register_shutdown_function('removePidFile');
+
+// 运行此文件
+include 'HttpFetch.class.php';
+$FetchGoods = new FetchGoods();
+$FetchGoods->run($argv);
+
+/** 
+ * 采集商品类
+ */
+class FetchGoods
+{
+	// 采集类
+	private $_httpFetch = null;
+
+	// 数据存放地址
+	public $data_dir = '';
+
+	// 站点名称
+	private $_sitename = '';
+
+	// 店铺名称
+	private $_shopname = '';
+
+	// 商品总数
+	private $_goods_total = 1;
+
+	public function __construct() 
+	{
+		// 数据存放目录
+		if (empty($this->data_dir)) {
+			$this->data_dir = dirname(__FILE__). '/data';
+		}
+	}
+
+	/**
+	 * 运行
+	 */
+	public function run($argv)
+	{
+		// 检查店铺地址是否正确
+		$shop_url = rtrim(trim($argv[1]), '/');
+		if (empty($shop_url)) {
+			echo "参数错误，请输入店铺地址\n";
+			return false;
+		}
+
+		// 匹配站点、店铺名称
+		$preg_name = '/(http:\/\/)?(\w+)\.(\w+)\./';
+		preg_match($preg_name, $shop_url, $matches);
+		$this->_shopname = $matches[2];
+		$this->_sitename = $matches[3];
+		
+		// 检查站点名称
+		if (!in_array($this->_sitename, array('taobao', 'tmall'))) {
+			echo "暂不支持 '{$this->_sitename}' 站点采集，只支持淘宝、天猫\n";
+			return false;
+		}
+
+		// 开始执行采集店铺商品
+		$this->fetchOneShop($shop_url);
+	}
+
+	/**
+	 * 采集某个店铺商品
+	 */
+	public function fetchOneShop($shop_url)
+	{
+		$url_arr = parse_url($shop_url);
+		$shop_host = "{$url_arr['scheme']}://{$url_arr['host']}";
+		$search_url = "{$shop_host}/search.htm?&mid=w-6029587454-0&search=y";
+
+		$time_format = "m-d H:i:s";
+		$start_time = date($time_format);
+		echo "shop_url:{$shop_url} ... start_time:{$start_time} ... start!\n";
+
+		// 实例化http采集类
+		$this->_httpFetch = new HttpFetch();
+
+		// 淘宝店铺所有宝贝列表需要登录更新cookie才可以抓取
+		// 为了避免登录，采集店铺分类列表
+		if ($this->_sitename == 'taobao') {
+			// 获取店铺分类采集规则
+			$rule_shop_category = $this->_getRules($this->_sitename, 'shop_category');
+			$search_source = $this->_httpFetch->get($search_url);
+			$category_urls = $this->collectAll($search_source, $rule_shop_category);
+			$category_urls && $category_urls = array_flip(array_flip($category_urls));
+			$search_source = null;
+		} else {
+			$category_urls = array($search_url);
+		}
+		
+		// 执行页面商品采集
+		foreach ($category_urls as $category_url) {
+			echo "category_url:{$category_url}\n";
+			$this->_fetchPageGoods($category_url);
+		}
+
+		$end_time = date($time_format);
+		echo "shop_url:{$shop_url} ... end_time:{$end_time} ... finish!\n";
+		return true;
+	}
+
+	/**
+	 * 采集某个分类商品
+	 * 
+	 * @param string $category_url 分类url
+	 */
+	private function _fetchPageGoods($category_url)
+	{
+		// 暂停5s，尽量避免淘宝防采集功能
+		$sleep = 5;
+
+		// 获取商品ID 采集规则
+		$rule_goods_id = $this->_getRules($this->_sitename, 'goods_id');
+
+		// 最多采集20页
+		for ($page_num = 1; $page_num < 20; $page_num++) {
+			$page_url = "{$category_url}&pageNo={$page_num}";
+			$page_source = $this->_httpFetch->proxyGet($page_url);
+
+			// 该页没有找到商品，采集完成
+			if (false !== strpos($page_source, '<p class="item-not-found">')) {
+				break;
+			}
+			$goods_ids = $this->collectAll($page_source, $rule_goods_id);
+			$goods_ids = array_flip(array_flip($goods_ids));
+			$goods_num = count($goods_ids);
+			echo "page_url:{$page_url} ... page_goods_num:{$goods_num}\n";
+			foreach ($goods_ids as $k => $good_id) {
+				$num = $k + 1;
+				echo "goods_total:{$this->_goods_total} ... {$num}/{$goods_num} ... goods_id:{$good_id}\n";
+				$this->_getGoodsData($good_id);
+			}
+			
+			echo "sleep {$sleep}s\n";
+			sleep($sleep);
+		}
+		return true;
+	}
+
+	/**
+	 * 处理商品数据
+	 *
+	 * @param int $good_id 商品ID
+	 */
+	public function _getGoodsData($good_id)
+	{
+		$shop_dir = "{$this->data_dir}/{$this->_shopname}";
+		$good_info_file = "{$shop_dir}/{$good_id}.php";
+
+		// 站点名称
+		if ($this->_sitename == 'tmall') {
+			$good_url = "http://detail.tmall.com/item.htm?id={$good_id}";
+		} else {
+			$good_url = "http://item.taobao.com/item.htm?id={$good_id}";
+		}
+		echo "good_url:{$good_url} ... ";
+
+		// 检查商品数据文件是否存在，存在不再重复采集
+		if (file_exists($good_info_file)) {
+			echo "exists!\n";
+			return;
+		}
+		
+		$good_source = $this->_httpFetch->proxyGet($good_url);
+		$good_source = mb_convert_encoding($good_source, "UTF-8", "GBK");
+		
+		// 获取站点采集规则
+		$rules = $this->_getRules($this->_sitename);
+
+		// 数据数组
+		$data = array();
+		$params = array('name', 'intro', 'shop_intro', 'price');
+		foreach ($params as $v) {
+			$data[$v] = $this->collectOne($good_source, $rules[$v]);
+		}
+
+		// 检查采集是否成功
+		if (empty($data['name'])) {
+			echo "failure!\n";
+			return;
+		}
+
+		// 获取商品描述
+		$data['desc'] = '';
+		$goods_desc_url = $this->collectOne($good_source, $rules['desc_url']); 
+		if ($goods_desc_url) {
+			$goods_desc = $this->_httpFetch->get($goods_desc_url);
+			$goods_desc = ltrim($goods_desc, "var desc='");
+        	$goods_desc = rtrim($goods_desc, "';");
+        	$data['desc'] = mb_convert_encoding(stripcslashes($goods_desc), 'UTF-8', 'GBK');
+		}
+
+		// 检查目录是否存在
+		$good_image_dir = "{$shop_dir}/img/{$good_id}";
+		$this->checkDir($good_image_dir, true);
+
+		// 匹配商品图片
+		$data['product_img'] = array();
+		$data['goods_img'] = $this->collectAll($good_source, $rules['product_img']);
+		if ($data['goods_img']) {
+			$goods_img_num = count($data['goods_img']);
+			echo "img_num:{$goods_img_num} ... ";
+			foreach ($data['goods_img'] as $img_url) {
+				// 获取图片名称
+				$img_name = end(explode('/', $img_url));
+				$good_image = "img/{$good_id}/{$img_name}";
+				$data['product_img'][] = $good_image;
+
+				// 保存图片
+				$img_file = "{$shop_dir}/{$good_image}";
+				file_put_contents($img_file, $this->_httpFetch->get($img_url));
+			}
+		}
+		
+		echo "ok\n";
+
+		// save info
+		file_put_contents($good_info_file, "<?php\nreturn ". var_export($data, true). ";\n");
+
+		// 已采集商品总数
+		$this->_goods_total++;
+
+		return true;
+	}
+
+	/**
+	 * 获取站点采集规则
+	 *
+	 * @param string $site_name 站点名称
+	 * @param string $name 某个站点规则名称
+	 * @return string|array 指定规则名称返回规则字符串，否返回某个站点规则
+	 */
+	private function _getRules($site_name='taobao', $name='')
+	{
+		static $rules;
+
+		if (empty($rules)) {
+			// 淘宝
+			$rules['taobao'] = array(
+				'page_total' => '/class="page-info"\>\d+\/(\d+)\<\/span\>/', // 单页商品数目
+				//'search-result' => '/class="search-result"\>\s+共搜索到\<span\>\s+(\d+)\s+\<\/span\>/', // 店铺总商品数
+				'shop_category' => '/(http:\/\/.+?\/category-\d+\.htm\?)search=y&catName/',
+				'goods_id' => '/class="item-name"\shref="http:\/\/item\.taobao\.com\/item\.htm\?id=(\d+)"/', // 商品ID
+				'name' => '/\<h3 class="tb-item-title"\>(.+?)\<\/h3\>/is', // 商品名称
+				'price' => '/\<em\sclass="tb-rmb-num"\>(.+?)\<\/em\>/', // 商品价格
+				'intro' => '/\<ul\sclass="attributes-list"\>(.+?)\<\/ul\>/is', // 商品简介
+				'product_img' => '/src="(http:\/\/.+?)_60x60\.jpg/', // 商品图片
+				'desc_url' => '/"(http:\/\/dsc\.taobaocdn\.com\/.+?)"/', // 商品描述url
+				'shop_intro' => '/\<p\sclass="base-info"\>(.+?)\<\/p\>/is', // 店铺简介
+			);
+
+			// 天猫
+			$rules['tmall'] = array(
+				'page_total' => '/class="ui-page-s-len"\>\d+\/(\d+)\<\/b\>/', // 总页数
+				'goods_id' => '/class="item.+?data-id="(\d+)"/is', // 商品ID
+				'name' => '/\<title\>(.+?)-tmall.+?\<\/title\>/is', // 商品名称
+				'price' => ' /class="J_originalPrice"\>(.+?)\<\/strong\>/', // 商品价格
+				'intro' => '/\<div\sclass="attributes-list"\sid="J_AttrList"\>(.+?)\<\/div\>\s+\<\/div\>/is', // 简介
+				'product_img' => '/(http:\/\/.+?)_60x60q90\.jpg/',
+				'desc_url' => '/"(http:\/\/dsc\.taobaocdn\.com\/.+?)"/', // 商品描述
+        		'shop_intro' => '/class="extend"\>(.+?)\<\/div\>/is', // 店铺简介
+        	);
+		}
+
+		$site_rules = $rules[$site_name];
+		return ($name) ? $site_rules[$name] : $site_rules;
+	}
+
+	/**
+	 * 匹配一条数据
+	 *
+	 * @param string $source 网页源码
+	 * @param string $preg 采集规则
+	 * @return string|bool 成功返回结果，失败返回false
+	 */
+	public function collectOne(&$source, $preg) 
+	{
+		if (preg_match($preg, $source, $matches)) {
+			return trim($matches[1]);
+		}
+		return false;
+	}
+
+	/**
+	 * 匹配多条数据
+	 *
+	 * @param string $source 页面源码
+	 * @param string $preg 采集规则
+	 * @return array 匹配成功返回数据，否返回空数组
+	 */
+	public function collectAll(&$source, $preg)
+	{
+		if (preg_match_all($preg, $source, $matches)) {
+			return $matches[1];
+		}
+		return array();
+	}
+
+	/**
+ 	 * 建立目录
+ 	 * 
+ 	 * @param      string     $dirname 目录名
+ 	 * @param      int        $mode 建立后的目录权限
+ 	 * @param      bool       $recursive 是否支持多级目录建立，默认否
+ 	 * @access     public
+ 	 * @return     bool       成功返回true，失败返回false
+ 	*/
+	public function createDir($dirname, $mode=0777, $recursive = false){
+		if (!$recursive) {
+			$ret = @mkdir($dirname, $mode);
+			if($ret) @chmod($dirname, $mode);
+			return $ret;
+		}
+		if (is_dir($dirname)) {
+			return true;
+		} elseif ($this->createDir(dirname($dirname), $mode, true)) {
+			$ret = @mkdir($dirname, $mode);
+			if($ret) @chmod($dirname, $mode);
+			return $ret;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+ 	 * 检查目录是否存在，不存在尝试自动建立
+ 	 * 
+ 	 * @param      string     $dirname 目录名
+ 	 * @param      bool       $autocreate 目录不存在是否尝试自动建立，默认否
+ 	 * @access     public
+ 	 * @return     bool       成功返回true，失败返回false
+ 	 */
+	public function checkDir($dirname, $autocreate=false){
+		if (is_dir($dirname)) {
+			return true;
+		} else {
+			if(empty($autocreate)) return false;
+			else return $this->createDir($dirname, 0777, true);
+		}
+	}
+}
---
+++ HttpFetch.class.php
@@ -0,0 +1,159 @@
+<?php
+/**
+ * http fetch class
+ */
+class HttpFetch
+{
+	// 代理IP列表
+	private $_proxylist = array();
+
+	// 记录失败代理IP
+	private $_failure_proxy = array();
+
+	// debug
+	public $debug = false;
+
+	public function __construct() 
+	{
+		// 获取代理IP
+		$this->_proxylist = $this->proxyList();
+	}
+
+	/**
+	 * 获取远程数据
+	 */ 
+	public function get($url)
+	{
+		return $this->disguise_curl($url);
+	}
+
+	/**
+	 * 通过代理方式获取页面内容
+	 *
+	 * @param string $url 链接地址
+	 * @return string $html 远程数据内容
+	 */
+	public function proxyGet($url)
+	{
+		// 尝试多次使用代理获取数据
+		for ($i = 0; $i < 3; $i++) {
+
+			// 获取随机代理IP
+			$proxy_ip = $this->_getRandProxy();
+
+			if ($this->debug) {
+				$start = microtime(true);
+			}
+			
+			// 获取远程内容
+			$html = $this->disguise_curl($url, $proxy_ip);
+
+			if ($this->debug) {
+				$len = strlen($html);
+				$used = microtime(true) - $start;
+				echo "proxy_ip:{$proxy_ip} ... used:{$used} ... len:{$len}\n";
+			}
+
+			if (!empty($html)) {
+				break;
+			}
+
+			// 记录失败IP信息
+			if (!isset($this->_failure_proxy[$proxy_ip])) {
+				$this->_failure_proxy[$proxy_ip] = 0;
+			}
+			$this->_failure_proxy[$proxy_ip] += 1;
+		}
+
+		// 代理获取失败，尝试本机获取
+		if (empty($html)) {
+			$html = $this->disguise_curl($url);
+		}
+
+		// 重置资源
+		//$this->_failure_proxy = array();
+
+		return $html;
+	}
+
+	/**
+	 * 获取一个随机代理IP
+	 */
+	private function _getRandProxy()
+	{
+		$proxy_count = count($this->_proxylist);
+		// 尝试多次获取随机代理IP，如果ip有失败记录，换其他ip
+		for (;;) {
+			// 获取随机代理IP
+			$rand_key = mt_rand(0, $proxy_count - 1);
+			$proxy_ip = $this->_proxylist[$rand_key];
+			if (!isset($this->_failure_proxy[$proxy_ip])) {
+				break;
+			}
+		}
+
+		return $proxy_ip;
+	}
+
+	/**
+	 * 代理IP列表
+	 */
+	public function proxyList()
+	{
+		$proxy_arr = array(
+			'58.20.127.100:3128', // 湖南省长沙市 联通
+			'218.108.168.69:82', // 浙江省杭州市
+			'218.108.170.166:80', // 浙江省杭州市
+			'139.210.98.86:8080', // 吉林省长春市联通
+			'218.108.168.68:82', // 浙江省杭州市
+			'111.161.126.84:80', // 天津市联通
+		);
+
+		return $proxy_arr;
+	}
+
+	/**
+	 * 模拟google抓取内容
+	 *
+	 * @link http://cn2.php.net/manual/en/function.curl-setopt.php#78046
+	 * @param string $url 链接地址
+	 * @param string $proxy 代理Ip信息，如：127.0.0.1:81
+	 * @return string $html 页面内容
+	 */
+	public function disguise_curl($url, $proxy=null) 
+	{ 
+		$curl = curl_init();
+
+  		// Setup headers - I used the same headers from Firefox version 2.0.0.6 
+  		// below was split up because php.net said the line was too long. :/ 
+		$header = array();
+		$header[0]  = "Accept: text/xml,application/xml,application/xhtml+xml,"; 
+		$header[0] .= "text/html;q=0.9,text/plain;q=0.8,image/png,*/*;q=0.5"; 
+		$header[] = "Cache-Control: max-age=0"; 
+		$header[] = "Connection: keep-alive"; 
+		$header[] = "Keep-Alive: 300"; 
+		$header[] = "Accept-Charset: ISO-8859-1,utf-8;q=0.7,*;q=0.7"; 
+		$header[] = "Accept-Language: en-us,en;q=0.5";
+  		$header[] = "Cookie: cna=l+pgC/cDdWcCAWVF+/5q+UVs; miid=3317872020099775032; __utma=6906807.434603219.1390489590.1390489590.1390489590.1; __utmz=6906807.1390489590.1.1.utmcsr=fuwu.taobao.com|utmccn=(referral)|utmcmd=referral|utmcct=/ser/detail.htm; x=e%3D1%26p%3D*%26s%3D0%26c%3D0%26f%3D0%26g%3D0%26t%3D0%26__ll%3D-1%26_ato%3D0; lzstat_uv=24182050841715729711|2581762@3201199@2945730@2948565@2798379@2043323@3045821@2805963@2738597@878758@2208862@3027305@3284827@2581759@2581747@2938535@2938538@2879138@3010391; ali_ab=60.186.203.48.1390489531936.4; l=asd890241::1397182032328::11; v=0; uc3=nk2=AmJam4TqFyWJ&id2=W8ncTY3m5dw%3D&vt3=F8dATHtpuP76kDFmmBg%3D&lg2=V32FPkk%2Fw0dUvg%3D%3D; existShop=MTM5NzYxMTM1NA%3D%3D; lgc=asd890241; tracknick=asd890241; sg=10d; cookie2=6b8940c7741e940084cc149db11f8b7c; mt=cp=0&np=&ci=1_1&cyk=0_0; cookie1=B0EwsR%2FpnjjSjqpUTsxO8woKrOXzJMXtv9vP4SUJA5I%3D; unb=80893640; t=43525df761cffb84c1297592f666dd75; publishItemObj=Ng%3D%3D; _cc_=UIHiLt3xSw%3D%3D; tg=0; _l_g_=Ug%3D%3D; _nk_=asd890241; cookie17=W8ncTY3m5dw%3D; pnm_cku822=117fCJmZk4PGRVHHxtNZngkZ3k%2BaC52PmgTKQ%3D%3D%7CfyJ6Zyd9OGcmY3YkZHYibx4%3D%7CfiB4D15%2BZH9geTp%2FJyN8PDJtLBMbCF4lHw%3D%3D%7CeSRiYjNhIHA3dWI0c2A4eGwmfz16PnhrNHJlMH1kJnc8bS1hfzoT%7CeCVoaEATTRBWFx1IEBRReHZYZg%3D%3D%7CeyR8C0obRRhYABdDABNTFAFGEU8XUxMFTgMVSREMSxxeG1MWCTMa%7CeiJmeiV2KHMvangudmM6eXk%2BAA%3D%3D; _tb_token_=3ee5e3b8e7690; uc1=lltime=1397569800&cookie14=UoLVYuvN1ebuMw%3D%3D&existShop=true&cookie16=Vq8l%2BKCLySLZMFWHxqs8fwqnEw%3D%3D&cym=1&cookie21=URm48syIZJTgtchfymSXVA%3D%3D&tag=3&cookie15=VFC%2FuZ9ayeYq2g%3D%3D";
+  		$header[] = "Pragma: "; // browsers keep this blank. 
+
+  		if (!is_null($proxy)) {
+  			curl_setopt ($curl, CURLOPT_PROXY, $proxy); 
+  		}
+
+  		curl_setopt($curl, CURLOPT_URL, $url); 
+  		curl_setopt($curl, CURLOPT_USERAGENT, 'Googlebot/2.1 (+http://www.google.com/bot.html)'); 
+  		curl_setopt($curl, CURLOPT_HTTPHEADER, $header); 
+  		curl_setopt($curl, CURLOPT_REFERER, 'http://www.google.com'); 
+  		curl_setopt($curl, CURLOPT_ENCODING, 'gzip,deflate'); 
+  		curl_setopt($curl, CURLOPT_AUTOREFERER, true); 
+  		curl_setopt($curl, CURLOPT_FOLLOWLOCATION, true);
+  		curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1); 
+  		curl_setopt($curl, CURLOPT_TIMEOUT, 10);
+
+  		$html = curl_exec($curl); // execute the curl command 
+  		curl_close($curl); // close the connection 
+
+  		return $html; // and finally, return $html 
+  }
+}
